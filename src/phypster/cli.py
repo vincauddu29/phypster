06e9453551b2302ad0e79b6fac31c2e1402e377d
vincent <vincauddu29@gmail.com>
2020-11-15 21:51:58 +0100

    add debug parameter "-d"

--- src/phypster/cli.py
+++ src/phypster/cli.py
@@ -1,43 +1,49 @@
-import argparse
+import argparse, json, inquirer
 from .phypster import Phypster
+
+
+import setuptools as _
 
 def main():
     #############
     # argsparse #
     #############
     parser = argparse.ArgumentParser(description='Flask starter')
-    parser.add_argument('--import_json', help='import json model')
+    parser.add_argument('--import_json', type=str, help='import json model')
+    parser.add_argument('-d', '--debug', action='store_true', help="enable debug log during the application generation")
 
     args = parser.parse_args()
+    phypster = Phypster()
 
-    print(args)
+    phypster.DEBUG = args.debug
 
-    # phypster = Phypster()
+    print("DEBUG = {0}".format(phypster.DEBUG))
 
-    # if args.import_json != None:
-    #     with open("mocks.json", "r") as f:
-    #         data_json = json.load(f)
+    if args.import_json != None:
+        with open("mocks.json", "r") as f:
+            data_json = json.load(f)
 
-    #     for data_enum in data_json["enums"]:
-    #         phypster.createEnum(data_enum)
+        for data_enum in data_json["enums"]:
+            phypster.createEnum(data_enum)
 
-    #     for data_entity in data_json["entities"]:
-    #         phypster.createEntity(data_entity)
-    # else:
-    #     cond_stop = False
-    #     choose_step = [inquirer.List('choose', message="What now", choices=['Create entity', 'Create enum', 'Stop'])]
+        for data_entity in data_json["entities"]:
+            phypster.createEntity(data_entity)
+    else:
+        cond_stop = False
+        choose_step = [inquirer.List('choose', message="What now", choices=['Create entity', 'Create enum', 'Stop'])]
 
-    #     while not cond_stop:
-    #         step_answer = inquirer.prompt(choose_step)
+        while not cond_stop:
+            step_answer = inquirer.prompt(choose_step)
 
-    #         if step_answer['choose'] == 'Create entity':
-    #             phypster.createEntity()
-    #         elif step_answer['choose'] == 'Create enum':
-    #             phypster.createEnum()
-    #         else:
-    #             cond_stop = True
-    # phypster.init()
-    # phypster.generateFiles()
+            if step_answer['choose'] == 'Create entity':
+                phypster.createEntity()
+            elif step_answer['choose'] == 'Create enum':
+                phypster.createEnum()
+            else:
+                cond_stop = True
+
+    phypster.init()
+    phypster.generateFiles()
 
 if __name__ == "__main__":
     main()
